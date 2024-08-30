056413fce0246e238b9e6ac20a9061129ffd44eb
dgrunsted1 <dgrunsted1@gmail.com>
2024-08-29 22:37:39 -0500

    alert z index

--- src/lib/components/alerts.svelte
+++ src/lib/components/alerts.svelte
@@ -23,7 +23,7 @@
 
 
 
-<div role="alert" id="alert" class="drop-shadow-xl alert {alert_types[type]} fixed top-24 transition-[left] {show ? 'left-0' : '-left-full'} duration-700 p- md:p-10 ml-5 w-4/5 md:w-96 flex flex-row">
+<div role="alert" id="alert" class="z-10 drop-shadow-xl alert {alert_types[type]} fixed top-24 transition-[left] {show ? 'left-0' : '-left-full'} duration-700 md:p-10 ml-5 w-4/5 md:w-96 flex flex-row">
     {#if type == "warning"}
             <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
     {:else if type == "error"}
