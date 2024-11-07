25ba169f61458e4bdfbb2f9ba1fbeff4a3b50a6c
dgrunsted1 <dgrunsted1@gmail.com>
2024-11-06 21:58:06 -0600

    my menus loading

--- src/routes/my_menus/+page.svelte
+++ src/routes/my_menus/+page.svelte
@@ -360,14 +360,14 @@
         </div>
     {#if user_menus.length > 0 || loading || no_results_found}
         <div id="menus" class="h-[calc(100svh-60px)] md:h-[calc(100svh-90px)] overflow-y-auto border border-primary rounded-md md:border-none w-full">
-            {#if no_results_found}
+            {#if loading}
+                <div class="text-center flex flex-col justify-center items-center space-y-5 mx-2 md:mx-auto md:text-4xl h-full w-full"><span class="loading loading-bars loading-lg"></span></div>
+            {:else if no_results_found}
                 <div class="flex flex-col justify-center items-center space-y-5 bg-base-200 mx-2 md:mx-auto p-16 border-2 border-base-300 rounded-md shadow-md  md:text-4xl mt-[30vh] max-w-md">
                     <div class="w-full flex justify-center content-center h-full">
                         no results
                     </div>
                 </div>
-            {:else if loading}
-                <div class="text-center flex flex-col justify-center items-center space-y-5 mx-2 md:mx-auto md:text-4xl h-full w-full"><span class="loading loading-bars loading-lg"></span></div>
             {:else}
                 {#each user_menus as curr, i}
                     <div id={user_menus[i].id} class="card md:card-side card-bordered bg-base-200 shadow-xl h-24 my-1.5 mx-1 cursor-pointer" onclick={show_menu_modal} onkeypress={show_menu_modal}>
