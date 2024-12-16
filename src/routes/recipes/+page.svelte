2728a99556f945c981066ab848fbbb29f3aa9511
Dave <dgrunsted1@gmail.com>
2024-12-16 17:13:25 -0600

    tweaks

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -443,7 +443,7 @@
             {/each}
             {#if !categories.length && !cuisines.length && !countries.length && !authors.length}
                 {#each Array(40) as _, i}
-                    <button id="country" class="btn btn-xs"><div class="skeleton h-2 w-12"></div></button> 
+                    <div id="country" class="btn btn-xs"><div class="skeleton h-2 w-12"></div></div> 
                 {/each}
             {/if}
         </div>
@@ -518,7 +518,7 @@
                     {/each}
                 {:else}
                     {#each Array(10) as _, i}
-                        <div class="card card-side bg-base-200 h-24 md:h-28 card-bordered border-primary cursor-pointer mx-1" onkeydown={window.location = `/cook_recipe/${item.url_id}/${item.servings}`} onclick={window.location = `/cook_recipe/${item.url_id}/${item.servings}`}>
+                        <div class="card card-side bg-base-200 h-24 md:h-28 card-bordered border-primary cursor-pointer mx-1">
                             <div class="flex items-center gap-4 w-full m-5">
                                 <div class="skeleton h-20 md:h-24 w-1/4 shrink-0 rounded-full"></div>
                                 <div class="flex flex-col gap-4 w-3/4">
@@ -581,7 +581,7 @@
             {/each}
             {#if !categories.length && !cuisines.length && !countries.length && !authors.length}
                 {#each Array(7) as _, i}
-                    <button id="country" class="btn btn-xs"><div class="skeleton h-2 w-12"></div></button> 
+                    <div id="country" class="btn btn-xs"><div class="skeleton h-2 w-12"></div></div> 
                 {/each}
             {/if}
         </div>
