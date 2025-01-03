ea18e4148a13c5044f28fb4d3792a0afb99afaf7
Dave <dgrunsted1@gmail.com>
2025-01-02 22:45:53 -0600

    menu card finally fixed

--- src/lib/components/menu_card.svelte
+++ src/lib/components/menu_card.svelte
@@ -24,30 +24,38 @@
 </script>
 {#if menu?.expand?.recipes}
     <!-- svelte-ignore a11y_no_static_element_interactions-->
-    <div id={menu.id} class="card md:card-side card-bordered border-primary bg-base-200 h-24 my-1.5 mx-1 cursor-pointer" onclick={handle_click} onkeypress={handle_click}>
-        <figure class="h-24 w-full md:w-1/2 shrink-0 flex overflow-hidden">
+    <div id={menu.id} class="card lg:card-side card-bordered border-primary bg-base-200 h-24 mx-1 cursor-pointer" onclick={handle_click} onkeypress={handle_click}>
+        <figure class="h-1/2 lg:h-24 w-full lg:w-1/2 shrink-0 flex overflow-hidden">
             {#each menu.expand.recipes.slice(0,6) as recipe, j}
                 {#if menu.expand.recipes[j].image}
-                    <img class="h-24 flex-1 min-w-0 object-cover" src={menu.expand.recipes[j].image} alt={menu.expand.recipes[j].title}/>
+                    <img class="lg:h-full flex-1 min-w-0 object-cover" src={menu.expand.recipes[j].image} alt={menu.expand.recipes[j].title}/>
                 {/if}
             {/each}
         </figure>
-        <div class="card-body flex flex-row justify-evenly content-center p-1 w-1/2 grow-0">
-            <div class="flex flex-col justify-between content-center h-full">
-                <div class="flex flex-row justify-evenly text-xs md:text-md items-center h-full space-x-2 md:space-x-5">
-                    <div class="text-center max-h-4 md:max-h-16 line-clamp-1 md:line-clamp-4 shrink">{menu.title}</div>
-                    <div class="text-center min-w-16 md:w-20 shrink-0 line-clamp-1">{format_date(menu.created)}</div>
+        <div class="card-body flex flex-row justify-between h-full content-center p-1 w-full lg:w-1/2 shrink lg:shrink-0 pt-0">
+            <div class="flex w-full space-x-1">
+                <div class="flex flex-col justify-between content-center lg:h-full mt-0 basis-full min-w-0">
+                    <div class="flex flex-row justify-evenly text-xs lg:text-md items-center h-full min-w-0">
+                        <div class="card-title text-xs lg:text-sm text-ellipsis overflow-hidden line-clamp-3 max-h-4 lg:max-h-16 min-w-0">{menu.title}</div>
+                        <div class="text-center line-clamp-1 min-w-16 shrink-0">{format_date(menu.created)}</div>
+                    </div>
+                    <div class="flex w-full">
+                        <p class="text-[10px] lg:text-[12px] border border-primary text-ellipsis whitespace-nowrap overflow-hidden h-fit pl-1 text-nowrap text-center basis-5 grow rounded-tl rounded-bl">{menu.expand.recipes.length} recipes</p>
+                        <p class="text-[10px] lg:text-[12px] border border-primary text-ellipsis whitespace-nowrap overflow-hidden h-fit pl-1 text-nowrap text-center basis-5 grow shrink">{get_grocery_list(menu, menu.servings, menu.sub_recipes).length} ingredients</p>
+                        <p class="text-[10px] lg:text-[12px] border border-primary text-ellipsis whitespace-nowrap overflow-hidden h-fit pl-1 text-nowrap text-center basis-5 grow shrink">{get_servings(menu.expand.recipes, menu.sub_recipes, menu.servings)} servings</p>
+                        <p class="text-[10px] lg:text-[12px] border border-primary text-ellipsis whitespace-nowrap overflow-hidden h-fit pl-1 text-nowrap text-center basis-5 grow shrink rounded-tr rounded-br">{get_total_time(menu.expand.recipes).display}</p>
+                    </div>
                 </div>
-                <div class="flex flex-row justify-evenly">
-                    <p class="text-center text-[10px] xl:text-[12px] border border-primary px-1 text-ellipsis whitespace-nowrap text-nowrap overflow-hidden rounded-tl rounded-bl">{menu.expand.recipes.length} recipes</p>
-                    <p class="text-center text-[10px] xl:text-[12px] border border-primary px-1 text-ellipsis whitespace-nowrap text-nowrap overflow-hidden">{get_grocery_list(menu, menu.servings, menu.sub_recipes).length} ingredients</p>
-                    <p class="text-center text-[10px] xl:text-[12px] border border-primary px-1 text-ellipsis whitespace-nowrap text-nowrap overflow-hidden">{get_servings(menu.expand.recipes, menu.sub_recipes, menu.servings)} servings</p>
-                    <p class="text-center text-[10px] xl:text-[12px] border border-primary px-1 text-ellipsis whitespace-nowrap text-nowrap overflow-hidden rounded-tr rounded-br">{get_total_time(menu.expand.recipes).display}</p>
+                <div class="flex conten-center items-center mr-1">
+                    <button id={menu.id} class="btn btn-sm p-1 btn-accent"  onclick={handle_delete}><DeleteIcon/></button>
                 </div>
-            </div>
-            <div class="flex conten-center items-center w-1/5">
-                <button id={menu.id} class="btn btn-sm p-1 btn-accent"  onclick={handle_delete}><DeleteIcon/></button>
             </div>
         </div>
     </div>
+
+    <!-- starting new card -->
+
+
+
+    
 {/if}
--- src/routes/my_menus/+page.svelte
+++ src/routes/my_menus/+page.svelte
@@ -46,7 +46,7 @@
         alert.type = "error";
         alert.show = true;
     }
-    
+
     function show_menu_modal(e){
         const is_mobile = (window.getComputedStyle(document.getElementById("desktop_menu")).display == "none") ? true : false;
         let id = e.id;
@@ -221,7 +221,7 @@
             />
         </div>
     {#if user_menus.length > 0 || loading || no_results_found}
-        <div id="menus" class="h-[calc(100svh-55px)] md:h-[calc(100svh-90px)] overflow-y-auto rounded-md md:border-none w-full">
+        <div id="menus" class="h-[calc(100svh-55px)] md:h-[calc(100svh-90px)] overflow-y-auto rounded-md md:border-none w-full space-y-2">
             {#if loading}
                 <div class="text-center flex flex-col justify-center items-center space-y-5 mx-2 md:mx-auto md:text-4xl h-full w-full"><span class="loading loading-bars loading-lg"></span></div>
             {:else if no_results_found}
