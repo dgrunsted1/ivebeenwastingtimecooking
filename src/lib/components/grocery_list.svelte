49f971846b521ede804744293b3bdeec0bfa70ec
dgrunsted1 <dgrunsted1@gmail.com>
2025-03-18 23:59:06 -0500

    remove touch none

--- src/lib/components/grocery_list.svelte
+++ src/lib/components/grocery_list.svelte
@@ -225,7 +225,7 @@
         {/if}
     </div>
     <div class="md:mx-3">
-        <div class="grocery_list touch-none h-[calc(100svh-150px)] md:h-[calc(100svh-105px)] overflow-y-auto px-2 py-4">
+        <div class="grocery_list h-[calc(100svh-150px)] md:h-[calc(100svh-105px)] overflow-y-auto px-2 py-4">
             {#if grocery_list.length > 0}
                 {#each grocery_list as item, i}
                     {#if edit}
