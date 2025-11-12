985d2c674654146506b8ba96cdb9f7e347655ca0
dgrunsted1 <dgrunsted1@gmail.com>
2025-11-11 20:52:05 -0600

    remove console.logs

--- src/lib/merge_ingredients.js
+++ src/lib/merge_ingredients.js
@@ -693,7 +693,6 @@ const areUnitsCompatible = (unit1, unit2) => {
 export const merge = function(ingrs) {
 //   console.log("Merging", ingrs);
   let grocery_list = [];
-  
   for (let item of ingrs) {
     if (!item.name) continue;
     
@@ -750,8 +749,7 @@ export const merge = function(ingrs) {
       
       // Replace the existing item
       grocery_list[matchIndex] = mergedItem;
-      console.log("Combining", bestMatch, "with", item, "to get", mergedItem);
-      
+
     } else {
       // Add as new item
       const newItem = {
@@ -769,6 +767,5 @@ export const merge = function(ingrs) {
       grocery_list.push(newItem);
     }
   }
-  
   return grocery_list;
 };
