8cc9fb46bcbfbe5e4c40f237cd4f86a5ee91e313
dgrunsted1 <dgrunsted1@gmail.com>
2025-11-11 23:33:13 -0600

    fixes

--- src/lib/ingr_to_groc.js
+++ src/lib/ingr_to_groc.js
@@ -51,10 +51,8 @@ export function get_grocery_item_name(name){
     removed = removed.replace(/\s\s+/g, ' ');
     const cleanDoc = nlp(removed);
     let mainNoun = cleanDoc.out('text').trim();
-    if (mainNoun === '' && name) {
-        console.log("name", name, "cleaned", cleaned, "text", text, "texta", texta, "textb", textb, "textc", textc, "textd", textd, "removed", removed, nlp(name).nouns().out('text'))
-    }
-    return mainNoun;
+    if (mainNoun) return mainNoun;
+    return name;
 }
 
 export function merge_grocery_items(ingr_list){
